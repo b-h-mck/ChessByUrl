3ba26175801fe617237e461d35b5fc7cd810cef9
b-h-mck <57464284+b-h-mck@users.noreply.github.com>
2025-02-17 20:45:09 +1300

    Put CSS and JS in separate files

---
+++ ChessByUrl/Pages/Play.cshtml.cs
@@ -0,0 +1,22 @@
+using ChessByUrl.Parser;
+using ChessByUrl.Rules;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChessByUrl.Pages
+{
+    public class PlayModel : PageModel
+    {
+        public IRuleset? Ruleset { get; set; } 
+        public Board? Board { get; set; } 
+        public IEnumerable<Move>? Moves { get; set; } 
+
+        public void OnGet(string? ruleset, string? state, string? moves)
+        {
+            var parsers = ParserCollection.Instance;
+            Ruleset = parsers.ParseRuleset(ruleset ?? "");
+            Board = Ruleset != null ? parsers.ParseBoard(Ruleset, state ?? "") : null;
+            Moves = Ruleset != null && Board != null ? parsers.ParseMoves(Ruleset, Board, moves ?? "") : null;
+        }
+    }
+}
---
+++ ChessByUrl/wwwroot/js/chess.js
@@ -0,0 +1,9 @@
+﻿function highlightSquare(cell) {
+    console.log("highlight", cell)
+    if (cell.classList.contains('selected')) {
+        cell.classList.remove('selected');
+    } else {
+        document.querySelectorAll('.selected').forEach(e => e.classList.remove('selected'));
+        cell.classList.add('selected');
+    }
+}
